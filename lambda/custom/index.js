6ec92ba87a6c16ba7ad2f69deac21f5bb76f1ca3
Fabrice Payet <hello@fabricepayet.fr>
2019-05-01 16:47:12 +0400

    remove helloWorld handler

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -64,23 +64,6 @@ const SauceIntentHandler = {
   }
 };
 
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "HelloWorldIntent"
-    );
-  },
-  handle(handlerInput) {
-    const speechText = "Hello World!";
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard("Hello World", speechText)
-      .getResponse();
-  }
-};
-
 const HelpIntentHandler = {
   canHandle(handlerInput) {
     return (
@@ -154,7 +137,6 @@ exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     SauceIntentHandler,
-    HelloWorldIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
