d22f10ef780c92f2bedaeaa8ce7083e2ecf2037f
Fabrice Payet <hello@fabricepayet.fr>
2019-05-05 16:50:04 +0400

    use dynamodb scan to get the sauces

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,28 +1,23 @@
 const Alexa = require("ask-sdk-core");
-const helpers = require("./helpers.js");
 var AWS = require("aws-sdk");
 const util = require("util");
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-function getSauceFromDatabase(callback) {
-  const meals = [];
+function getSaucesForMeal(meal, callback) {
   const params = {
     TableName: "sauce",
-    IndexName: "id-NameSauce-index",
-    Limit: 100,
-    KeyConditionExpression: "id = :id",
-    ExpressionAttributeValues: {
-      ":id": 1
-    }
+    Limit: 3,
+    FilterExpression: "contains(#Meals, :v)",
+    ExpressionAttributeNames: { "#Meals": "Meals" },
+    ExpressionAttributeValues: { ":v": meal }
   };
 
-  docClient.query(params, function(err, data) {
+  docClient.scan(params, function(err, data) {
     if (err) {
       callback(err);
     } else {
-      data.Items.forEach(result => meals.push(...result.Meals));
-      callback(null, meals);
+      callback(null, data.Items);
     }
   });
 }
@@ -32,8 +27,7 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === "LaunchRequest";
   },
   handle(handlerInput) {
-    const speechText =
-      "Demandez moi avec quelle sauce vous pourriez accompagner un repas";
+    const speechText = "Pour quel repas cherchez vous une sauce ?";
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -46,18 +40,38 @@ const SauceIntentHandler = {
   canHandle(handlerInput) {
     return (
       handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "sauce"
+      handlerInput.requestEnvelope.request.intent.name === "Meal"
     );
   },
   async handle(handlerInput) {
     let speechOutput = "";
-    const meals = await util.promisify(getSauceFromDatabase)();
-    if (meals.indexOf("agneau") === -1) {
-      speechOutput += "Je ne trouve pas de sauce pour ce plat";
+    const intent = handlerInput.requestEnvelope.request.intent;
+    const mealValue = intent.slots.Repas.value;
+    const sauceDocs = await util.promisify(getSaucesForMeal)(mealValue);
+    if (sauceDocs.length) {
+      if (sauceDocs.length > 1) {
+        const saucesNames = sauceDocs
+          .map(sauce => sauce.NameSauce)
+          .toString()
+          .replace(",", " ou ");
+
+        speechOutput =
+          "J'ai trouvé plusieurs sauces pouvant accompagner ce plat : " +
+          saucesNames +
+          ". Laquelle préférez-vous ?";
+      } else {
+        const sauceName = sauceDocs[0].NameSauce;
+        speechOutput =
+          "Je vous propose la sauce " + sauceName + " pour accompagner ce plat";
+      }
     } else {
-      speechOutput = "J'ai trouvé une sauce pour ce plat";
+      speechOutput += "Je ne trouve pas de sauce pour ce plat. A la prochaine.";
+      return handlerInput.responseBuilder.speak(speechOutput).getResponse();
     }
-    return handlerInput.responseBuilder.speak(speechOutput).getResponse();
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      .getResponse();
   }
 };
 
@@ -69,12 +83,11 @@ const HelpIntentHandler = {
     );
   },
   handle(handlerInput) {
-    const speechText = "You can say hello to me!";
+    const speechText = "Je peux vous aider";
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard("Hello World", speechText)
       .getResponse();
   }
 };
@@ -119,8 +132,8 @@ const ErrorHandler = {
     console.log(`Error handled: ${error.message}`);
 
     return handlerInput.responseBuilder
-      .speak("Désolé je n'arrive pas à comprendre la commande.")
-      .reprompt("Désolé je n'arrive pas à comprendre la commande.")
+      .speak("Une erreur s'est produite, veillez vérifier les journaux")
+      .reprompt("Une erreur s'est produite, veillez vérifier les journaux")
       .getResponse();
   }
 };
